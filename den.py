c867413c757e921f66afce9aacc5d66cf1cac024
Jiasheng Wu <jiashenw@usc.edu>
2019-05-28 10:29:17 -0700

    Update den.py

--- den.py
+++ den.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python3
 import sys, os, re
 
 if __name__ == '__main__':
 
-    # from input, find url in format '...playlist\.m3u8'
+	# from input, find url in format '...playlist\.m3u8'
     link_url = sys.argv[1]
     playlist_url = re.match(r'.*playlist\.m3u8', link_url).group()
     
@@ -10,5 +11,11 @@ if __name__ == '__main__':
     m = re.search(r'([A-Z]+[0-9]+_)([0-9]+)', link_url)
     video_name = m.group(1) + m.group(2)[-8:] + '.mp4'
     
-    # use ffmpeg to download
-    os.system('ffmpeg -i %s -c copy -bsf:a aac_adtstoasc %s' % (playlist_url, video_name))
+    # use _ffmpeg_ to download
+    os.system('ffmpeg -i %s -c copy -bsf:a aac_adtstoasc ~/Downloads/%s' % (playlist_url, video_name))
+
+    # use _autosub_ to translate subtitle, if possible
+    # try:  	
+    #     os.system('autosub ~/Downloads/%s' % (video_name))
+    # except Exception as e:
+    # 	raise e
